e62f87578b1d3991acef89665572690f931328f0
arash.r400@gmail.com <arash.r400@gmail.com>
2020-09-28 16:53:08 +0330

    react app created

---
+++ first-rp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2><i>Hello ! How are you ??</i></h2>
+    </div>
+  );
+}
+
+export default App;
