ef6778a32adf2595e37d3c119da1c0de3fcf11a0
arash.r400@gmail.com <arash.r400@gmail.com>
2020-10-11 12:51:35 +0330

    something changed

--- first-rp/src/App.js
+++ first-rp/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h2><i>Hello ! How are you ??</i></h2>
-    </div>
-  );
+    return (
+        <div className="App">
+            
+            <button type="button" class="btn btn-success">Success</button>
+            <button type="button" class="btn btn-danger">Danger</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ first-rp/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {render} from "react-dom";
+
+
+export class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello im home component !!
+            </div>
+        );
+    }
+
+}
